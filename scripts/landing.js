c58e25c4e24774633f3ca75956e0ca91c5a24ecf
Jeffrey King <jdking33@aol.com>
2016-09-03 10:21:33 -0700

    Submit Checkpoint 23-Events

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,6 +1,6 @@
-var animatePoints = function() {
-  
-  var points = document.getElementsByClassName('point');
+var pointsArray = document.getElementsByClassName('point');
+
+var animatePoints = function(points) {
   
   var revealPoint = function(indexNumber) {
     points[indexNumber].style.opacity = 1;
@@ -15,4 +15,15 @@ var animatePoints = function() {
   
 };
 
-animatePoints();
+window.onload = function() {
+  if(window.innerHeight > 950) {
+    animatePoints(pointsArray);
+  }
+  var sellingPoints = document.getElementsByClassName('selling-points')[0];
+  var scrollDistance = sellingPoints.getBoundingClientRect().top - window.innerHeight + 200;
+  window.addEventListener('scroll', function(event) {
+    if(document.documentElement.scrollTop || document.body.scrollTop >= scrollDistance) {
+      animatePoints(pointsArray);
+    }
+  });
+}
